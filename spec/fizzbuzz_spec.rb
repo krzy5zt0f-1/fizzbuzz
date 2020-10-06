e089f977878a5be91427406038cc30ae80419024
zoethomas <zoethomas1540@gmail.com>
2020-10-06 12:10:24 +0100

    passed first test and failed on 4

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,3 +5,9 @@ require 'fizzbuzz'
      expect(fizzbuzz(3)).to eq 'fizz'
    end
  end
+
+ describe 'fizzbuzz' do
+   it 'returns "fizz" when passed 3' do
+     expect(fizzbuzz(4)).to eq 4
+   end
+ end
