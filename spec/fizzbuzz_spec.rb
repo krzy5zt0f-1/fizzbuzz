f6c1be1f8fa00cf46a9c69631205ebe2ef353a27
zoethomas <zoethomas1540@gmail.com>
2020-10-07 10:34:31 +0100

    Added extra tests for each situation

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,6 @@
  # useless comment
 require 'fizzbuzz'
+# This section checks for divisibility by 3
  describe 'fizzbuzz' do
    it 'returns "fizz" when passed 3' do
      expect(fizzbuzz(3)).to eq 'fizz'
@@ -7,19 +8,46 @@ require 'fizzbuzz'
  end
 
  describe 'fizzbuzz' do
-   it 'returns "fizz" when passed 4' do
-     expect(fizzbuzz(4)).to eq 4
+   it 'returns "fizz" when passed 6' do
+     expect(fizzbuzz(6)).to eq 'fizz'
    end
  end
 
+ # This section checks for divisibility by 5
  describe 'fizzbuzz' do
    it 'returns "buzz" when passed 5' do
      expect(fizzbuzz(5)).to eq 'buzz'
    end
  end
 
-describe 'fizzbuzz' do
-  it 'returns `fizzbuzz` when passed 15' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
-  end
-end
+ describe 'fizzbuzz' do
+   it 'returns "buzz" when passed 10' do
+     expect(fizzbuzz(10)).to eq 'buzz'
+   end
+ end
+
+# This section checks for divisibility by 3 and 5
+ describe 'fizzbuzz' do
+   it 'returns `fizzbuzz` when passed 15' do
+     expect(fizzbuzz(15)).to eq 'fizzbuzz'
+   end
+ end
+
+ describe 'fizzbuzz' do
+   it 'returns `fizzbuzz` when passed 30' do
+     expect(fizzbuzz(30)).to eq 'fizzbuzz'
+   end
+ end
+
+ # This section checks for divisibility by neither 3 or 5
+ describe 'fizzbuzz' do
+   it 'returns "fizz" when passed 4' do
+     expect(fizzbuzz(4)).to eq 4
+   end
+ end
+
+ describe 'fizzbuzz' do
+   it 'returns "fizz" when passed 100' do
+     expect(fizzbuzz(109)).to eq 109
+   end
+ end
